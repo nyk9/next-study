dc55c6e5d8fece79a02d2339c88440eb3ed77cd8
nyk9 <eijikagimoto@gmail.com>
2024-12-11 16:11:00 +0900

    feat: App routerの動画の機能を実装

---
+++ src/app/app-router/new/page.tsx
@@ -0,0 +1,29 @@
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default function New() {
+
+  const createTodo = async (formData: FormData) => {
+    "use server";
+
+    const title = formData.get("title");
+    await fetch("http://localhost:3001/todos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title }),
+        });
+    revalidatePath("/app-router");
+    redirect("/app-router");
+  }
+  return (
+    <div>
+      <h1>新規作成</h1>
+      <form action={createTodo}>
+        <input type="text" name="title"/>
+        <button type="submit">作成</button>
+      </form>
+    </div>
+  );
+}
